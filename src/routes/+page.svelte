5fe2860f4a175517523984c65cb8d9dce3d99ed4
sujang958 <sujangdev@gmail.com>
2023-10-05 21:19:53 +0900

    feat: add designs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,47 @@
-<section class="text-center">
-  <p class="text-4xl font-bold">Smash or Pass</p>
-</section>
+<div class="flex flex-col items-center w-full">
+	<div class="py-24 max-w-5xl w-full self-center relative">
+		<section class="text-center py-8">
+			<p class="text-6xl font-bold">Smash or Pass</p>
+			<p class="text-lg text-neutral-400 mt-6">
+				Smash, next question? There are 10+ smash or pass questions
+			</p>
+		</section>
+
+		<div class="py-4" />
+
+		<section class="flex flex-row items-center justify-between">
+			<div class="flex flex-row items-center gap-x-3">
+				<button type="button" class="bg-neutral-950 rounded-xl px-3 py-1">
+					<p class="text-base font-medium">🔥 Popular</p>
+				</button>
+				<button type="button" class="bg-neutral-800 rounded-xl px-3 py-1">
+					<p class="text-base font-medium">🆕 Latest</p>
+				</button>
+			</div>
+			<div class="relative">
+				<input
+					type="text"
+					class="bg-neutral-800 rounded-xl w-64 px-2.5 py-1"
+					placeholder="Search some shit"
+				/>
+			</div>
+		</section>
+
+		<div class="py-8" />
+
+		<section class="grid grid-cols-3 gap-6">
+			<button type="button" class="bg-black/20 rounded-xl p-4 flex flex-col items-start">
+				<img src="/kana.png" alt="" class="object-contain w-full rounded-xl" />
+				<p class="text-2xl font-semibold mt-4">128 anime girls</p>
+				<p class="text-xs mt-1 pb-1 text-neutral-400">696,969 people smashed</p>
+			</button>
+		</section>
+	</div>
+</div>
+
+<a
+	href="/new"
+	class="fixed top-6 right-8 px-4 py-2 rounded-xl hover:bg-white/10 transition duration-200 cursor-pointer"
+>
+	New
+</a>

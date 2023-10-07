a1f7a93201e16d9d930e61e527e6e9dd9e5e1e0d
sujang958 <sujangdev@gmail.com>
2023-10-08 00:03:51 +0900

    docs: add a comment

---
+++ src/routes/s/[id]/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col items-center">
+	<div class="h-screen max-w-5xl w-full flex flex-col items-center p-24">
+		<p class="text-4xl font-bold">128 Anime Girls</p>
+		<p class="text-base">1/128 done</p>
+	</div>
+</div>
+<!-- TODO: add squoosh to handle uploaded images on client and deny the images over 4 mb -->

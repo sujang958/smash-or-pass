6b43105f51ed2c793cd0459f6a4717371d82428a
sujang958 <sujangdev@gmail.com>
2023-10-09 23:06:26 +0900

    ref: add some things to do

--- apps/backend/src/routes/v1/v1.ts
+++ apps/backend/src/routes/v1/v1.ts
@@ -2,7 +2,7 @@ import { FastifyPluginCallback } from "fastify"
 import { V1_GAMES } from "./games.js"
 
 export const V1: FastifyPluginCallback = (fastify, opts, done) => {
-  fastify.register(V1_GAMES)
+  fastify.register(V1_GAMES) // this doesnt work fuck
 
   fastify.setErrorHandler((err, req, reply) => {
     console.log(err)

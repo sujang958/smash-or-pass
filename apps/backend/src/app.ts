2c5b1dcfff898a559870aaf82c4f9ce6b771d01e
sujang958 <sujangdev@gmail.com>
2023-10-09 23:06:05 +0900

    fix: fix http2 errors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -6,11 +6,11 @@ import { V1 } from "./routes/v1/v1.js"
 
 if (!process.env.PORT || !process.env.FILE_PATH) process.exit(1)
 
-const app = fastify({ http2: true, logger: true })
+const app = fastify({ logger: true }) // In WSL, when http2 enabled, response 글자 깨짐
 
 app.register(multipart, { limits: { fileSize: 4.096e9, fieldSize: 4.096e6 } })
 app.register(V1, { prefix: "/v1" })
 
 app
   .listen({ port: Number(process.env.PORT) })
-  .then((v) => console.log(v, "Listening", process.env.PORT))
+  .then((v) => console.log(v, "Listening"))

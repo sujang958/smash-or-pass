34649b24ff9c27d40c0ca31f478f7ab31ae93ed6
sujang958 <sujangdev@gmail.com>
2023-11-11 17:05:23 +0900

    fix: fix not being dremoved properly

--- apps/frontend/src/routes/new/+page.svelte
+++ apps/frontend/src/routes/new/+page.svelte
@@ -21,12 +21,15 @@
       fileReader.readAsDataURL(file)
     })
 
-  let inputImages: Array<Promise<string>> = []
+  let inputImages: Array<{ file: Promise<string>; name: string }> = []
 
-  $: try {
-    if (files?.length > 0) for (const file of files) inputImages = [...inputImages, readFile(file)]
-  } catch (e) {
-    toast.error("Failed loading images")
+  const loadImages = () => {
+    try {
+      if (files?.length > 0)
+        for (const file of files) inputImages = [...inputImages, { file: readFile(file), name: "" }]
+    } catch (e) {
+      toast.error("Failed loading images")
+    }
   }
 
   const onSubmit = async (event: SubmitEvent) => {
@@ -34,19 +37,13 @@
 
     const data = new FormData(event.target)
 
-    if (fileNames.length !== files.length) return toast.error("A name is required for an image")
-    if (fileNames.filter((v) => !v).length > 0)
-      return toast.error("A name is required for an image")
-
-    for (const fileName of fileNames) data.append("imageNames", fileName)
+    for (const inputImage of inputImages) data.append("imageNames", inputImage.name)
 
     // TODO: change to env
     const json = await ky.post("http://localhost:3000/v1/games/new", { body: data }).json()
 
     console.log(json)
   }
-
-  const fileNames: string[] = []
 </script>
 
 <div class="flex flex-col items-center">
@@ -125,42 +122,28 @@
           accept="image/*"
           bind:this={fileInput}
           bind:files
+          on:change|self={() => loadImages()}
           class="opacity-0 top-8 left-0 absolute -z-10"
           required
         />
         <!-- invisible -->
       </label>
 
-      <!-- <p>Remember me</p> let server generate TODO: generate a safe password to modify later -->
       <div class="border-b border-neutral-800 mt-8 mb-4" />
-      <!-- <label class="text-base flex flex-row items-start gap-x-2 align-middle">
-        <input
-          type="checkbox"
-          name="rememberMe"
-          class="w-4 h-4 text-blue-600 rounded bg-neutral-700 border-gray-600 appearance-none mt-1"
-        />
-        <div>
-          <p>Remeber me</p>
-          <p class="text-xs text-neutral-400 mt-0.5">
-            By checking this, a password will be generated for you. You can modify this
-            smash-or-pass with the password after creating it.
-          </p>
-        </div>
-      </label> -->
+
       <button
         type="submit"
         class="rounded-lg text-base px-4 py-1 bg-white text-black font-medium mt-2">Submit</button
       >
     </form>
     <section class="flex flex-col gap-y-8 w-1/2 py-56">
-      {#each inputImages as inputFile, i}
-        {#await inputFile then src}
+      {#each inputImages as inputImage, i}
+        {#await inputImage.file then src}
           <div class="a{i} relative">
             <button
               type="button"
               class="absolute top-2 right-2 rounded-full p-0.5 backdrop-blur-md bg-black/50 transition duration-200 hover:scale-105"
               on:click={() => {
-                fileInput.value = ""
                 delete inputImages[i]
                 inputImages = inputImages.filter((v) => v)
               }}
@@ -180,7 +163,7 @@
               <p class="text-base font-medium">Name</p>
               <input
                 type="text"
-                bind:value={fileNames[i]}
+                bind:value={inputImage.name}
                 class="rounded-lg bg-neutral-800 border border-neutral-700 text-white px-2 py-0.5 text-sm"
                 placeholder="Type here"
               />
